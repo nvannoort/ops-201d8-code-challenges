b1dbc45da38cc7e5ef665d004489c601c7e0d8b6
nvannoort <nick.vannoort@gmail.com>
2023-04-26 22:06:40 +0000

    This is my commit for Ops 03

---
+++ ops201d8-day03-script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Script Name:                  Script Name
+# Author:                       Your Name
+# Date of latest revision:      00/00/0000
+# Purpose:                      Purpose
+
+# Declaration of variables
+
+# Declaration of functions
+
+function login_history {
+  echo "This is the login history:"
+  last
+}
+
+
+# Main
+
+login_history
+login_history
+login_history
+
+# End
