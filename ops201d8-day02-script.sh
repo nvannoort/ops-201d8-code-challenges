d840efe09e3dfe939e79feee61a9180d6a13cd04
nvannoort <nick.vannoort@gmail.com>
2023-04-26 17:37:40 +0000

    this is my commit

---
+++ ops201d8-day02-script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Script Name:                  Script Name
+# Author:                       Your Name
+# Date of latest revision:      00/00/0000
+# Purpose:                      Purpose
+
+# Declaration of variables
+
+# Declaration of functions
+
+
+# Main
+echo "Hello, world!"
+
+
+
+# End
